6a32cba450466e6bfe7cbbfa40f602562065a36f
BhanuByreddy <bhanubyreddy2008@gmail.com>
2025-10-16 14:04:18 +0530

    Delete app directory

--- app/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from app.models import Room
-
-class RoomSerializer(ModelSerializer):
-    class Meta:
-        model = Room
-        fields='__all__'
